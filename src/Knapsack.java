edc48642b49d25cb7bbad3ac423a186670a9d150
yanis <yann.amrouche@gmail.com>
2021-09-29 15:54:28 +0200

    Knapsack with branch & bound (1)

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -3,76 +3,9 @@ import java.util.*;
 
 public class Knapsack {
 
-    static int bagCapacity;
-    static int nbOfItems;
+    public static int bagCapacity;
+    public static int nbOfItems;
 
-    public static List<String> readDataFile(String filepath) throws IOException {
-
-        File file = new File(filepath);
-        FileReader fileReader = new FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        List<String> data = new ArrayList<>();
-        String line;
-        while ((line = bufferedReader.readLine()) != null){
-            data.add(line);
-        }
-        fileReader.close();
-        return data;
-    }
-
-    public static double[][] loadData(String filepath) throws IOException {
-
-        List<String> data = readDataFile(filepath);
-        bagCapacity = Integer.parseInt(data.get(0));
-        data = data.subList(1,data.size());
-        nbOfItems = data.size();
-        double[] profits = new double[data.size()];
-        double[] weights = new double[data.size()];
-        for(int i=0; i<data.size(); i++){
-            String[] s = data.get(i).split(" ");
-            weights[i] = Integer.parseInt(s[0]);
-            profits[i] = Integer.parseInt(s[1]);
-        }
-        double[][] matrix = new double[3][nbOfItems];
-        for(int i=0; i < nbOfItems; i++){
-            matrix[0][i] = profits[i];
-            matrix[1][i] = weights[i];
-            matrix[2][i] = profits[i]/weights[i];
-        }
-        return matrix;
-
-    }
-
-   public static void swap(double[][] mat, int i, int j){
-        double tmp = mat[i][j-1];
-        mat[i][j-1] = mat[i][j];
-        mat[i][j] = tmp;
-   }
-   public static double[][] sortingByRatio(double[][] mat){
-        //double[][] newMat = new double[3][nbOfItems];
-        for(int i=0; i < nbOfItems; i++){
-            for (int j=1; j < (nbOfItems - i); j++){
-                if(mat[2][j-1] < mat[2][j]){
-                    swap(mat,0,j);//swap the first row
-                    swap(mat,1,j);//swap the second row
-                    swap(mat,2,j);
-
-                }
-            }
-        }
-        return mat;
-   }
-
-    public static void printMatrix(double[][] mat){
-        for(int i=0; i<mat.length; i++){
-            System.out.print("| ");
-            for (int j=0; j<mat[0].length; j++){
-                System.out.print(mat[i][j]+" ");
-            }
-            System.out.print(" |");
-            System.out.println();
-        }
-    }
 
     /* ======================================================================= */
 
@@ -193,19 +126,9 @@ public class Knapsack {
 
     }
 
-    public static void main(String args[]) throws IOException {
-        double[][] Mat = loadData(args[0]);
-        System.out.println("la masse totale du sac = "+bagCapacity);
-        System.out.println("le nombre d'objets dans le manoir = "+nbOfItems);
-        Item[] items = new Item[nbOfItems];
-        for(int i=0; i < nbOfItems; i++){
-            items[i] = new Item((int) Mat[0][i],Mat[1][i],i);
-        }
-        findOptimalSolution(items);
-
-
-    }
-
+    /* $ javac Knapsack.java
+       $ java Knapsack [chemin_vers_le_fichier_sac]
+     */
 
 
 
--- src/Parser.java
+++ src/Parser.java
@@ -1,2 +1,58 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parser {
+
+
+    public static List<String> readDataFile(String filepath) throws IOException {
+
+        File file = new File(filepath);
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        List<String> data = new ArrayList<>();
+        String line;
+        while ((line = bufferedReader.readLine()) != null){
+            data.add(line);
+        }
+        fileReader.close();
+        return data;
+    }
+
+    public static double[][] loadData(String filepath) throws IOException {
+
+        List<String> data = readDataFile(filepath);
+        Knapsack.bagCapacity = Integer.parseInt(data.get(0));
+        data = data.subList(1,data.size());
+        Knapsack.nbOfItems = data.size();
+        double[] profits = new double[data.size()];
+        double[] weights = new double[data.size()];
+        for(int i=0; i < data.size(); i++){
+            String[] s = data.get(i).split(" ");
+            weights[i] = Integer.parseInt(s[0]);
+            profits[i] = Integer.parseInt(s[1]);
+        }
+        double[][] matrix = new double[3][Knapsack.nbOfItems];
+        for(int i=0; i < Knapsack.nbOfItems; i++){
+            matrix[0][i] = profits[i];
+            matrix[1][i] = weights[i];
+            matrix[2][i] = profits[i]/weights[i];
+        }
+        return matrix;
+
+    }
+
+    public static void printMatrix(double[][] mat){
+        for(int i=0; i<mat.length; i++){
+            System.out.print("| ");
+            for (int j=0; j<mat[0].length; j++){
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.print(" |");
+            System.out.println();
+        }
+    }
 }
